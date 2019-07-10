b080418c3cb0b7cc4a850530c3ff03d0e7736a15
nmaekawa <nmaekawa@g.harvard.edu>
2019-07-10 12:46:47 -0400

    define STATIC_ROOT

--- sock/settings.py
+++ sock/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.environ.get(
+    'SOCK_STATIC_ROOT', os.path.join(BASE_DIR, 'static/'))
 
 # channels
 ASGI_APPLICATION = 'sock.routing.application'
